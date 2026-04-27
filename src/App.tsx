612ef256f15bf2d3eb0fa377345b574b8d3f61ad
Weber <weber@pop-os.Home>
2026-04-27 20:44:34 +0100

    fix: estrutura do router

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,49 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Hero from './components/Hero';
 import RecipeGrid from './components/RecipeGrid';
 import Services from './pages/Services';
 
+// Componente de Layout para manter a Navbar fixa em todas as páginas
+function RootLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <>
-        <Navbar />
-        <Hero />
-        <RecipeGrid />
-      </>
-    ),
-  },
-  {
-    path: "/receitas",
-    element: (
-      <>
-        <Navbar />
-        <RecipeGrid />
-      </>
-    ),
-  },
-  {
-    path: "/servicos",
-    element: (
-      <>
-        <Navbar />
-        <Services />
-      </>
-    ),
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <Hero />
+            <RecipeGrid />
+          </>
+        ),
+      },
+      {
+        path: "/receitas",
+        element: <RecipeGrid />,
+      },
+      {
+        path: "/servicos",
+        element: <Services />,
+      },
+      {
+        path: "/contactos",
+        element: <div className="py-20 text-center font-serif text-2xl">Página de Contactos em Construção 🚧</div>,
+      },
+    ],
   },
 ]);
 
-function App() {
+export default function App() {
   return <RouterProvider router={router} />;
 }
-
-export default App;
